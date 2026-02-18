d2439c22fb0dacc120b447b2de49edc8e6f52b6d
11968-ship-it <11968@holbertonstudents.com>
2026-02-18 05:07:24 +0300

    Update 102-type_checking.py

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-"""Module that provides a function to zoom a sequence by repeating its elements."""
+"""Module that provides a function to zoom a sequence by repeating its
+elements.
+"""
 
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Return a list where each element of the input tuple is repeated 'factor' times.
+    Return a list where each element of the input tuple is repeated
+    'factor' times.
     """
     zoomed_in: List = [item for item in lst for _ in range(factor)]
     return zoomed_in
