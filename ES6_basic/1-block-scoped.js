5b3fac96b00a58e6c156039466a463213606c84d
11968-ship-it <11968@holbertonstudents.com>
2026-04-06 02:24:42 +0300

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;   // outer scope
-  let task2 = true;   // outer scope
+  let task = false;  // outer variable
+  let task2 = true;  // outer variable
 
   if (trueOrFalse) {
-    let task = true;   // block scoped, does NOT overwrite outer task
-    let task2 = false; // block scoped, does NOT overwrite outer task2
+    task = true;      // reassign outer variable
+    task2 = false;    // reassign outer variable
   }
 
   return [task, task2];
