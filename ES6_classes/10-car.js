3a0a488a01c9520f548f6cff890c9d2a48d7f4a3
11968-ship-it <11968@holbertonstudents.com>
2026-04-06 03:16:07 +0300

    Update 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,39 +1,16 @@
-Implement a class named Car:
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-Constructor attributes:
-brand (String)
-motor (String)
-color (String)
-Each attribute must be stored in an "underscore" attribute version (ex: name is stored in _name)
-Add a method named cloneCar. This method should return a new object of the class.
-Hint: Symbols in ES6
+  static get [Symbol.species]() {
+    return this;
+  }
 
-bob@dylan:~$ cat 10-main.js
-import Car from "./10-car.js";
-
-class TestCar extends Car {}
-
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
-
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
-
-console.log(tc1 == tc2);
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 10-main.js
-TestCar { _brand: 'Nissan', _motor: 'Turbo', _color: 'Pink' }
-true
-TestCar { _brand: undefined, _motor: undefined, _color: undefined }
-true
-false
-bob@dylan:~$ 
-Repo:
-
-GitHub repository: holbertonschool-web_back_end
-Directory: ES6_classes
-File: 10-car.js
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
