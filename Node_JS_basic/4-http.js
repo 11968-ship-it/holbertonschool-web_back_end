87b1f9d173179639d230e31a999d17147bf81cad
11968-ship-it <11968@holbertonstudents.com>
2026-04-06 04:11:54 +0300

    Update 4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,20 +1,11 @@
 const http = require('http');
 
-const PORT = 1245;
-const HOST = 'localhost';
-
-// Create the server
 const app = http.createServer((req, res) => {
-  // Set the response header to plain text
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
-  // Send the body content
   res.end('Hello Holberton School!');
 });
 
-// Start listening on port 1245
-app.listen(PORT, HOST);
+app.listen(1245);
 
-// Export the app variable
 module.exports = app;
